3c10e00fa2ddf56f4dad74269f057f116771ecc5
kirill1984221321-gif <kirill1984221321@gmail.com>
2026-08-18 23:30:53 +0300

    Fix shell-loader runtime checks disabled by Java assertions

--- shell-loader/src/main/java/com/vectras/vm/Loader.java
+++ shell-loader/src/main/java/com/vectras/vm/Loader.java
@@ -4,21 +4,60 @@ public class Loader {
     public static void main(String[] args) {
         try {
             android.content.pm.PackageInfo targetInfo = (android.os.Build.VERSION.SDK_INT <= 32) ?
-                    android.app.ActivityThread.getPackageManager().getPackageInfo(BuildConfig.APPLICATION_ID, android.content.pm.PackageManager.GET_SIGNATURES, 0) :
-                    android.app.ActivityThread.getPackageManager().getPackageInfo(BuildConfig.APPLICATION_ID, (long) android.content.pm.PackageManager.GET_SIGNATURES, 0);
-            assert targetInfo != null : BuildConfig.packageNotInstalledErrorText.replace("ARCH", android.os.Build.SUPPORTED_ABIS[0]);
-            assert targetInfo.signatures.length == 1 && BuildConfig.SIGNATURE == targetInfo.signatures[0].hashCode() : BuildConfig.packageSignatureMismatchErrorText;
-            android.util.Log.i(BuildConfig.logTag, "loading " + targetInfo.applicationInfo.sourceDir + "::" + BuildConfig.CLASS_ID + "::main of " + BuildConfig.APPLICATION_ID + " application (commit " + BuildConfig.COMMIT + ")");
-            Class<?> targetClass = Class.forName(BuildConfig.CLASS_ID, true,
-                    new dalvik.system.PathClassLoader(targetInfo.applicationInfo.sourceDir, null, ClassLoader.getSystemClassLoader()));
+                    android.app.ActivityThread.getPackageManager().getPackageInfo(
+                            BuildConfig.APPLICATION_ID,
+                            android.content.pm.PackageManager.GET_SIGNATURES,
+                            0) :
+                    android.app.ActivityThread.getPackageManager().getPackageInfo(
+                            BuildConfig.APPLICATION_ID,
+                            (long) android.content.pm.PackageManager.GET_SIGNATURES,
+                            0);
+
+            if (targetInfo == null) {
+                throw new IllegalStateException(
+                        BuildConfig.packageNotInstalledErrorText.replace("ARCH", preferredAbi()));
+            }
+
+            if (targetInfo.signatures == null || targetInfo.signatures.length != 1
+                    || BuildConfig.SIGNATURE != targetInfo.signatures[0].hashCode()) {
+                throw new IllegalStateException(BuildConfig.packageSignatureMismatchErrorText);
+            }
+
+            android.util.Log.i(
+                    BuildConfig.logTag,
+                    "loading " + targetInfo.applicationInfo.sourceDir
+                            + "::" + BuildConfig.CLASS_ID
+                            + "::main of " + BuildConfig.APPLICATION_ID
+                            + " application (commit " + BuildConfig.COMMIT + ")");
+
+            Class<?> targetClass = Class.forName(
+                    BuildConfig.CLASS_ID,
+                    true,
+                    new dalvik.system.PathClassLoader(
+                            targetInfo.applicationInfo.sourceDir,
+                            null,
+                            ClassLoader.getSystemClassLoader()));
             targetClass.getMethod("main", String[].class).invoke(null, (Object) args);
-        } catch (AssertionError e) {
+        } catch (IllegalStateException e) {
             System.err.println(e.getMessage());
         } catch (java.lang.reflect.InvocationTargetException e) {
-            e.getCause().printStackTrace(System.err);
+            Throwable cause = e.getCause();
+            if (cause != null) {
+                cause.printStackTrace(System.err);
+            } else {
+                e.printStackTrace(System.err);
+            }
         } catch (Throwable e) {
             android.util.Log.e(BuildConfig.logTag, "Loader error", e);
             e.printStackTrace(System.err);
         }
+    }
+
+    private static String preferredAbi() {
+        String[] supportedAbis = android.os.Build.SUPPORTED_ABIS;
+        if (supportedAbis != null && supportedAbis.length > 0 && supportedAbis[0] != null) {
+            return supportedAbis[0];
+        }
+        return "unknown";
     }
 }
